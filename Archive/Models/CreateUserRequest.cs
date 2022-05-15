4c27f822da7d121e8498ed1a2fc715cfde233fc2
Group1IDB2001 <project3afa@gmail.com>
2022-05-15 16:41:12 +0300

    исправил CreateUserRequset

--- Archive/Models/CreateUserRequest.cs
+++ Archive/Models/CreateUserRequest.cs
@@ -1,11 +1,24 @@
-﻿namespace Archive.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Archive.Models
 {
     public class CreateUserRequest
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required.")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Pleace enter valid email.")]
         public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Role is required.")]
         public int Role { get; set; }
     }
 }
