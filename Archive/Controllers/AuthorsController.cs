5a0d834035b0b7c75c767598726667e342a550e1
Group1IDB2001 <project3afa@gmail.com>
2022-05-19 11:57:21 +0300

    Author add

--- Archive/Controllers/AuthorsController.cs
+++ Archive/Controllers/AuthorsController.cs
@@ -16,64 +16,14 @@ namespace Archive.Controllers
         }
 
 
-        [HttpPut]
-        [Route("authors")]
-        public async Task AddAuthor([FromBody] CreateAuthorRequest request) => await _manager.AddAuthor(request.Name, request.Born, request.Death, request.About);
-
         [HttpGet]
-        [Route("authors")]
-        public async Task<IList<Author>> GetAllAuthors() => await _manager.GetAllAuthors();
-
-
-        [HttpDelete]
-        [Route("authors/{id:int}")]
-        public async Task DeleteAuthor(int id) => await _manager.DeleteAuthor(id);
-
-
-        [HttpGet]
-        [Route("authors/{id:int}")]
-        public async Task<Author> GetAuthorById(int id) => await _manager.GetAuthorById(id);
-
-        [HttpGet]
-        [Route("authors/itemid/{itemid:int}")]
-        public async Task<IList<Author>> GetAuthorsByItemId(int itemId) => await _manager.GetAuthorsByItemId(itemId);
-
-
-        [HttpGet]
-        [Route("authors/{name}")]
-        public async Task<Author> GetAuthorByName(string name) => await _manager.GetAuthorByName(name);
-
-        [HttpGet]
-        [Route("authors/year/{year:int}")]
-        public async Task<IList<Author>> GetAuthorsByYear(int year) => await _manager.GetAuthorsByYear(year);
-
-        [HttpPut]
-        [Route("authors/name/{id:int}")]
-        public async Task EditAuthorName(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorName(id, request.Name);
-
-        [HttpPut]
-        [Route("authors/born/{id:int}")]
-        public async Task EditAuthorBorn(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorBorn(id, request.Born);
-
-        [HttpPut]
-        [Route("authors/death/{id:int}")]
-        public async Task EditAuthorDeath(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorDeath(id, request.Death);
-
-        [HttpPut]
-        [Route("authors/about/{id:int}")]
-        public async Task EditAuthorAbout(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorAbout(id, request.About);
-
-        //Page
-        
-
-        
-
-        public async Task<IActionResult> Index(int pg =1)
+        [Route("Authors")]
+        public async Task<IActionResult> Index(int pg = 1)
         {
             var authors = await _manager.GetAllAuthors();
             int counter = authors.Count();
             const int pagesize = 2;
-            if(pg < 1) pg = 1;
+            if (pg < 1) pg = 1;
 
             var pager = new Pager(counter, pg, pagesize);
 
@@ -85,5 +35,60 @@ namespace Archive.Controllers
 
             return View(data);
         }
+
+
+        //[HttpPut]
+        //[Route("authors")]
+        //public async Task AddAuthor([FromBody] CreateAuthorRequest request) => await _manager.AddAuthor(request.Name, request.Born, request.Death, request.About);
+
+        //[HttpGet]
+        //[Route("authors")]
+        //public async Task<IList<Author>> GetAllAuthors() => await _manager.GetAllAuthors();
+
+
+        //[HttpDelete]
+        //[Route("authors/{id:int}")]
+        //public async Task DeleteAuthor(int id) => await _manager.DeleteAuthor(id);
+
+
+        //[HttpGet]
+        //[Route("authors/{id:int}")]
+        //public async Task<Author> GetAuthorById(int id) => await _manager.GetAuthorById(id);
+
+        //[HttpGet]
+        //[Route("authors/itemid/{itemid:int}")]
+        //public async Task<IList<Author>> GetAuthorsByItemId(int itemId) => await _manager.GetAuthorsByItemId(itemId);
+
+
+        //[HttpGet]
+        //[Route("authors/{name}")]
+        //public async Task<Author> GetAuthorByName(string name) => await _manager.GetAuthorByName(name);
+
+        //[HttpGet]
+        //[Route("authors/year/{year:int}")]
+        //public async Task<IList<Author>> GetAuthorsByYear(int year) => await _manager.GetAuthorsByYear(year);
+
+        //[HttpPut]
+        //[Route("authors/name/{id:int}")]
+        //public async Task EditAuthorName(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorName(id, request.Name);
+
+        //[HttpPut]
+        //[Route("authors/born/{id:int}")]
+        //public async Task EditAuthorBorn(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorBorn(id, request.Born);
+
+        //[HttpPut]
+        //[Route("authors/death/{id:int}")]
+        //public async Task EditAuthorDeath(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorDeath(id, request.Death);
+
+        //[HttpPut]
+        //[Route("authors/about/{id:int}")]
+        //public async Task EditAuthorAbout(int id, [FromBody] CreateAuthorRequest request) => await _manager.EditAuthorAbout(id, request.About);
+
+        ////Page
+        
+
+        
+
+        
     }
 }
